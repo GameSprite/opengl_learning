b970aa1e7fe1e809e8cd064307546e2a4583d50c
chenguanzhou <1668831812@qq.com>
2018-01-25 23:27:58 +0800

    更符合人体物理的灰度(人眼感知 g > r > b)

--- framebuffer/screen.fs
+++ framebuffer/screen.fs
@@ -5,6 +5,6 @@ out vec4 color;
 void main()
 {
     color = texture(texture1,texCoords);
-    float average = (color.r + color.g + color.b) / 3.0;
+    float average = 0.2126 * color.r + 0.7152 * color.g + 0.0722 * color.b;
     color = vec4(average,average,average,1.0);
 }
