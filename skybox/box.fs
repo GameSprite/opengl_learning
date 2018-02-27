8b60adbb1dbd3405c265a1fa6cce9dc0b0bcb05a
GameSprite <1668831812@qq.com>
2018-02-27 15:10:14 +0800

    箱子作为玻璃，折射天空盒景象。

--- skybox/box.fs
+++ skybox/box.fs
@@ -8,7 +8,9 @@ uniform samplerCube tex0;
 uniform vec3 cameraPos;
 void main()
 {
+    //折射指数的比率
+    float ratio = 1.0 / 1.52;//从空气射入玻璃
     vec3 viewDir = normalize(Position - cameraPos);
-    vec3 reflectDir = reflect(viewDir,normalize(Normal));
-    color = texture(tex0,reflectDir);
+    vec3 refractDir = refract(viewDir,normalize(Normal),ratio);//折射光线，也就是眼睛透过玻璃箱子，看到的天空盒的纹理坐标
+    color = texture(tex0,refractDir);
 }
